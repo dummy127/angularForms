0532c79a7a4f21cd3dc05295ce14418a4457f0be
user.email <huzaifaansari9270@gmail.com>
2025-05-31 08:58:45 +0530

    31-05-2025

--- Desktop/newangular/angularForms/src/app/app.component.ts
+++ Desktop/newangular/angularForms/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Form1Component } from './form1/form1.component';
 import { Form2Component } from './form2/form2.component';
+import { Form3Component } from './form3/form3.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,Form1Component,Form2Component],
+  imports: [RouterOutlet,Form1Component,Form2Component,Form3Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
